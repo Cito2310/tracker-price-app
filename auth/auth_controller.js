ef27358f58548f14b68a17138f8c7a19ade532c3
Cito2310 <cito2310@gmail.com>
2022-10-03 18:32:31 -0300

    More fast check register - Encrypt password in the database

--- auth/auth_controller.js
+++ auth/auth_controller.js
@@ -1,29 +1,33 @@
 // IMPORTS
-// const bcryptjs = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 const jwt = require('jsonwebtoken');
-// const User = require("./user_model");
+
 const User = require("./user_model")
 
 // const token = jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256'});
 
 // CONTROLLERS
-const login = (req, res) => {
-    res.json({"HOLA":"HOLA"})
+const register = async(req, res) => {
+    const { username, password, email } = req.body;
+    
+    // checks
+    if (await User.findOne({$or: [{email},{username}]})) {return res.status(400).json({ msg : "This email or username is already registered" })};
+    
+    // create new user
+    const newUser = new User({ username, password, email });
+    
+    // password encrypt
+    const salt = bcryptjs.genSaltSync();
+    newUser.password = bcryptjs.hashSync( password , salt);
+    
+    // response
+    await newUser.save();
+    res.json( newUser );
 }
 
 
-const register = async(req, res) => {
-    const { username, password, email } = req.body;
-
-    // checks
-    if (await User.findOne({username})) {return res.status(400).json({ msg : "This username already exists" })}
-    if (await User.findOne({email})) {return res.status(400).json({ msg : "This email is already registered" })}
-
-    // response
-    const newUser = new User({ username, password, email });
-    await newUser.save()
-
-    res.json( newUser )
+const login = (req, res) => {
+    res.json({"HOLA":"HOLA"})
 }
 
 
